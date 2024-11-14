6114215833fee6895092c517c2d0916a4a182ac6
seungtaekhong <hsk4991149@naver.com>
2024-11-15 00:45:13 +0900

    add practice: syn, nonblock

--- practice/Synchronous_NonBlocking/Synchronous1.java
+++ practice/Synchronous_NonBlocking/Synchronous1.java
@@ -12,29 +12,8 @@ public class Synchronous1 implements Strategy {
 
     @Override
     public void call() {
-        boolean[] M = new boolean[2], N = new boolean[2];
-
-        M[0] = A.nioRead();
-        B.C();
-        N[0] = B.nioRead();
-
-        while (!(M[1] && N[1])) {
-            if (!M[0]) {
-                M[0] = A.nioRead();
-            }
-            else if (!M[1]) {
-                A.A();
-                A.B();
-                M[1] = true;
-            }
-
-            if (!N[0]) {
-                N[0] = B.nioRead();
-            }
-            else if (!N[1]) {
-                B.D();
-                N[1] = true;
-            }
-        }
+        // write your code!
+        // TaskA: nioRead() -> A() -> B()
+        // TaskB: C() -> nioRead() -> D()
     }
 }
